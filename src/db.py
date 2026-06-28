e6688f94975f7694aa399b16b6ad0c506ff56d90
markusmeisel-star <markus.meisel@gmail.com>
2026-06-28 16:12:56 +0200

    Create db.py

---
+++ src/db.py
@@ -0,0 +1,19 @@
+import os
+from supabase import create_client
+
+def get_client():
+    url = os.environ["SUPABASE_URL"]
+    key = os.environ["SUPABASE_KEY"]
+    return create_client(url, key)
+
+def upsert_activity(db, activity):
+    db.table("activities").upsert(activity).execute()
+
+def upsert_daily_metrics(db, metrics):
+    db.table("daily_metrics").upsert(metrics).execute()
+
+def get_week_activities(db, week_start):
+    return db.table("activities")\
+        .select("*")\
+        .gte("started_at", week_start)\
+        .execute().data
